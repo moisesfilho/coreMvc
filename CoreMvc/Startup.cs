499b0c6e71ff56253d252808f35b4d1efd778945
Moisés Filho <moises.alves@sefaz.ce.gov.br>
2018-01-15 16:54:26 -0300

    Iniciando persistencia

--- CoreMvc/Startup.cs
+++ CoreMvc/Startup.cs
@@ -6,6 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using FluentNHibernate.Automapping;
+using FluentNHibernate.Cfg;
+using FluentNHibernate.Cfg.Db;
+using NHibernate;
+using NHibernate.Cfg;
+using NHibernate.Tool.hbm2ddl;
 
 namespace CoreMvc
 {
@@ -22,6 +28,29 @@ namespace CoreMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            // services.AddSingleton<NHibernate.ISessionFactory>(factory =>
+            // {
+            //     return Fluently
+            //         .Configure()
+            //         .Database(() =>
+            //         {
+            //             return FluentNHibernate.Cfg.Db.MsSqlConfiguration
+            //                 .MsSql2012
+            //                 .ShowSql()
+            //                 .ConnectionString("");
+            //         })
+            //         .Mappings(m => m.FluentMappings.AddFromAssemblyOf<Model>())
+            //         .BuildSessionFactory();
+            // });
+
+            // services.AddScoped<NHibernate.ISession>(factory =>
+            //     factory
+            //     .GetServices<NHibernate.ISessionFactory>()
+            //     .First()
+            //     .OpenSession()
+            // );
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
